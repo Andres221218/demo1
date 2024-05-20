da121867a61528bd661c5374a69cd0f4a1d23164
Andrés221218 <andreortiz579@gmail.com>
2024-05-20 11:01:26 -0500

    Avance1

--- src/main/java/org/holamundo/servlet/demo/ParametrosGetServlet.java
+++ src/main/java/org/holamundo/servlet/demo/ParametrosGetServlet.java
@@ -1,2 +1,34 @@
-package org.holamundo.servlet.demo;public class ParametrosGetServlet {
+package org.holamundo.servlet.demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/servletParametro")
+public class ParametrosGetServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        String param1 = req.getParameter("param1");
+        out.println("<!DOCTYPE html>");
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"UTF-8\">");
+        out.println("<title>Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Nombre \t "+param1+"</h1>");
+        out.println("</body>");
+        out.println("</html>");
+
+    }
+
 }
--- src/main/java/org/holamundo/servlet/demo/Servlet.java
+++ src/main/java/org/holamundo/servlet/demo/Servlet.java
@@ -1,2 +1,40 @@
-package org.holamundo.servlet.demo;public class Servlet {
+package org.holamundo.servlet.demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/servlet")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Seteo el tipo de contenido que va devolver
+        //el servidor
+        resp.setContentType("text/html");
+        /*definimos una variable del tipo PrintWriter
+         * Para mostrar el contenido en pantalla*/
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"UTF-8\">");
+        out.println("<title>Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>RESPUESTAS FORMULARIO</h1>");
+        out.println("<h2>NOMBRE:</h2>");
+        out.println("<h2>APELLIDO: </h2>");
+        out.println("<h2>EDAD</h2>");
+        out.println("<h2>CURSO</h2>");
+        out.println("<h2>GENERO</h2>");
+        out.println("<h2>LENGUAJE DE PROGRAMACION</h2>");
+        out.println("<h2>DESCRIPCION</h2>");
+        out.println("</body>");
+        out.println("</html>");
+    }
 }
